adb60b732504cb0509d747a302e75bdd3bfc08cb
dohyunkimmm <wsg9505@naver.com>
2026-09-18 07:19:05 +0900

    refactor(v2): route screen effects through controllers

--- src/v2/ui/screen-effects.js
+++ src/v2/ui/screen-effects.js
@@ -1,11 +1,12 @@
 import{observeActiveScreen}from'../core/screen-observer.js';
 
-export function installScreenEffects({scenarioStore,home,payment,secondary,onScreen}){
+export function installScreenEffects({scenarioStore,home,filterResults,payment,secondary,onScreen}){
   return observeActiveScreen((screenId,previous)=>{
     if(!screenId)return;
 
     scenarioStore.renderForScreen(screenId);
     home.onScreen(screenId,previous);
+    filterResults.onScreen(screenId,previous);
     payment.onScreen(screenId,previous);
     secondary.onScreen(screenId,previous);
     window.FootMateProductOps?.renderInspector?.();
