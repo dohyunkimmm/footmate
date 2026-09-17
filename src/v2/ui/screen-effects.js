fa5bd75ab9203c7b2cee92e05e2604a77033c8ee
dohyunkimmm <wsg9505@naver.com>
2026-09-18 07:18:14 +0900

    feat(v2): move screen effects to observer

---
+++ src/v2/ui/screen-effects.js
@@ -0,0 +1,14 @@
+import{observeActiveScreen}from'../core/screen-observer.js';
+
+export function installScreenEffects({scenarioStore,home,payment,secondary,onScreen}){
+  return observeActiveScreen((screenId,previous)=>{
+    if(!screenId)return;
+
+    scenarioStore.renderForScreen(screenId);
+    home.onScreen(screenId,previous);
+    payment.onScreen(screenId,previous);
+    secondary.onScreen(screenId,previous);
+    window.FootMateProductOps?.renderInspector?.();
+    onScreen?.(screenId,previous);
+  });
+}
